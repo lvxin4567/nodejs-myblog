8f07aba7e5ed6a8181ca7eaac55c65693166a39e
lvxin <517888030@qq.com>
2020-06-22 16:23:54 +0800

    1

---
+++ websocket_test/Server/startServer.bat
@@ -0,0 +1,3 @@
+start "hallServer" cmd /k node .\hallServer.js
+start "gameServer" cmd /k node .\gameServer.js
+PAUSE
--- websocket_test/testClient/assets/Script/hallSence.ts
+++ websocket_test/testClient/assets/Script/hallSence.ts
@@ -20,6 +20,9 @@ export default class hallScene extends cc.Component {
     @property(cc.Button)
     btn_send: cc.Button = null;
 
+    @property(cc.RichText)
+    test_rich:cc.RichText = null;
+
     ws: Socket = null;
 
     protected _wait: boolean = false;
@@ -35,6 +38,8 @@ export default class hallScene extends cc.Component {
 
         this.ws = new Socket('lobby');
         this.ws.doConnect({ ip: SOCKET_CONF_HALL.host, port: SOCKET_CONF_HALL.port });
+
+        this.test_rich.string = "<color=#00ff00>Rich</c><color=#0fffff>Text</color>";
 
         EventManager.getInstance().on("hallScene::test", (data) => {
             this.label.string = JSON.stringify(data);
@@ -53,23 +58,20 @@ export default class hallScene extends cc.Component {
         }, this);
 
         EventManager.getInstance().on("ws::msg::lobbyToGame", (data) => {
-            // eventQueue.getInstance().eventQueue.push({
-            //     caller: this, listener: (recdata) => {
-            //         if (this._wait) {
-            //             return false;
-            //         }
-            //         console.log(`收到服务器消息${JSON.stringify(recdata)}`);
-            //         // cc.director.loadScene("gameScene", (recdata) => {
-            //         //     console.log("recdata", recdata);
-            //         // })
-            //         this.jumpManager.setJumpData(recdata);
-            //         cc.director.loadScene("gameScene");
-            //         return true;
-            //     }, args: data
-            // })
-            console.log(`收到服务器消息${JSON.stringify(data)}`);
-            self.jumpManager.setJumpData(data);
-            cc.director.loadScene("gameScene");
+            eventQueue.getInstance().eventQueue.push({
+                caller: this, listener: (recdata) => {
+                    if (this._wait) {
+                        return false;
+                    }
+                    console.log(`收到服务器消息${JSON.stringify(recdata)}`);
+                    // cc.director.loadScene("gameScene", (recdata) => {
+                    //     console.log("recdata", recdata);
+                    // })
+                    this.jumpManager.setJumpData(recdata);
+                    cc.director.loadScene("gameScene");
+                    return true;
+                }, args: data
+            })
         }, this);
     }
 
